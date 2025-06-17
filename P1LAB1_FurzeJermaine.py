126ec0ef76c942373fe28fe4f0c3f022f4638fa1
Jermaine1002 <furzej9312@student.faytechcc.edu>
2025-06-16 20:32:39 -0400

    Add files via upload
    added file from previous assignment

---
+++ P1LAB1_FurzeJermaine.py
@@ -0,0 +1,11 @@
+# Jermai9ne Furze
+# 2025-06-15
+# P1LAB1 - Welcome Message
+# This program asks the user for their first and last name separately and displays a welcome message.
+
+# Get user input
+first_name = print("Enter your first name:Jermaine ")
+last_name = print("Enter your last name:Furze ")
+
+# Display welcome message
+print("Hello Jermaine Furze welcome to this course!")
